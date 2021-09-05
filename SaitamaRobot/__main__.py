cf930d998d3f28bd9a0f899228013a04a52af988
aigerakabea <89718270+valtaoibestblader@users.noreply.github.com>
2021-09-05 16:57:16 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -63,7 +63,7 @@ You can find my list of available commands with! Hit` *🔐Commands*
 buttons = [
     [
         InlineKeyboardButton(
-            text="[ADD SHU KURENAI TO YOUR GROUP]",url="t.me/VALTAOITHEBOT?startgroup=true"),
+            text="[ADD VALT AOI TO YOUR GROUP]",url="t.me/VALTAOITHEBOT?startgroup=true"),
     ],
     [
         InlineKeyboardButton(
