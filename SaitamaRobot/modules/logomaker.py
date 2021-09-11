8e2f22a26e0a72a9ac0d27c83a21e8bc39161cb3
aigerakabea <89718270+valtaoibestblader@users.noreply.github.com>
2021-09-11 16:00:53 +0530

    Update logomaker.py

--- SaitamaRobot/modules/logomaker.py
+++ SaitamaRobot/modules/logomaker.py
@@ -1,8 +1,25 @@
-from SaitamaRobot.events import register
-from SaitamaRobot import OWNER_ID
-from SaitamaRobot import telethn as tbot
-import os 
+from EmiliaAnimeBot.events import register
+from EmiliaAnimeBot import OWNER_ID
+from EmiliaAnimeBot import telethn as tbot
+import os
+import random
 from PIL import Image, ImageDraw, ImageFont
+
+
+logopics = [
+ 
+ "./EmiliaAnimeBot/SaitamaRobot/photo_2021-09-11_15-46-57.jpg"
+
+]
+
+logofonts = [
+ 
+ "./EmiliaAnimeBot/SaitamaRobot/Beyblade Metal Fight Font.ttf"
+ 
+]
+
+pic_choice = random.choice(logopics)
+font_choice = random.choice(logofonts)
 
 
 @register(pattern="^/logo ?(.*)")
@@ -11,7 +28,7 @@ async def lego(event):
  if event.sender_id == OWNER_ID:
      pass
  else:
-     
+
     if not quew:
        await event.reply('Provide Some Text To Draw!')
        return
@@ -20,13 +37,13 @@ async def lego(event):
  await event.reply('Creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
-    img = Image.open('./SaitamaRobot/resources/blackbg.jpg')
+    img = Image.open(pic_choice)
     draw = ImageDraw.Draw(img)
     image_widthz, image_heightz = img.size
     pointsize = 500
-    fillcolor = "gold"
+    fillcolor = "Blue"
     shadowcolor = "blue"
-    font = ImageFont.truetype("./SaitamaRobot/resources/Chopsic.otf", 330)
+    font = ImageFont.truetype(font_choice , 330)
     w, h = draw.textsize(text, font=font)
     h += int(h*0.21)
     image_width, image_height = img.size
@@ -34,61 +51,22 @@ async def lego(event):
     x = (image_widthz-w)/2
     y= ((image_heightz-h)/2+6)
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
-    fname2 = "LogoByYone.png"
+    fname2 = "Logo.png"
     img.save(fname2, "png")
     await tbot.send_file(event.chat_id, fname2, caption="Made By @ROHITH_NO_1")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
-   await event.reply(f'Error Report @VALTAOITHEBOT, {e}')
-
-
+   await event.reply(f'Error Report @PigasusSupport, {e}')
 
    
-@register(pattern="^/wlogo ?(.*)")
-async def lego(event):
- quew = event.pattern_match.group(1)
- if event.sender_id == OWNER_ID:
-     pass
- else:
-     
-    if not quew:
-       await event.reply('Provide Some Text To Draw!')
-       return
-    else:
-       pass
- await event.reply('Creating your logo...wait!')
- try:
-    text = event.pattern_match.group(1)
-    img = Image.open('./SaitamaRobot/resources/blackbg.jpg')
-    draw = ImageDraw.Draw(img)
-    image_widthz, image_heightz = img.size
-    pointsize = 500
-    fillcolor = "white"
-    shadowcolor = "blue"
-    font = ImageFont.truetype("./SaitamaRobot/resources/Maghrib.ttf", 1000)
-    w, h = draw.textsize(text, font=font)
-    h += int(h*0.21)
-    image_width, image_height = img.size
-    draw.text(((image_widthz-w)/2, (image_heightz-h)/2), text, font=font, fill=(255, 255, 255))
-    x = (image_widthz-w)/2
-    y= ((image_heightz-h)/2+6)
-    draw.text((x, y), text, font=font, fill="white", stroke_width=0, stroke_fill="white")
-    fname2 = "LogoByYone.png"
-    img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By @valtaoibladerbot")
-    if os.path.exists(fname2):
-            os.remove(fname2)
- except Exception as e:
-   await event.reply(f'Error Report  @valtaoibladerbo{e}')
-
 file_help = os.path.basename(__file__)
 file_help = file_help.replace(".py", "")
 file_helpo = file_help.replace("_", " ")
 
 
-__help__ = """
- ❍ /logo text :  Create your logo with your name
+__mod_name__ = "Logos"
 
- """
-__mod_name__ = "Logo"
+__help__ = """
+`/logo`*:* Create a logo with text!
+"""
